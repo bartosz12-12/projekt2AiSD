413d32a541ac9f6bf6ea151f3246801f63e7e807
bartosz12-12 <73654110+bartosz12-12@users.noreply.github.com>
2021-12-08 12:32:59 +0100

    Add files via upload

--- projekt.py
+++ projekt.py
@@ -22,7 +22,7 @@ class BinaryNode:
         self.left_child.parent = self
 
     def add_right_child(self,value):
-        self.right_child = BinaryNode(valu)
+        self.right_child = BinaryNode(value)
         self.right_child.parent = self
 
     def traverse_post_order(self, visit:Callable[['Any'],None]):
@@ -94,23 +94,64 @@ class BinaryTree:
             if self.right_child:
                 self.right_child.traverse_pre_order(visit)
 
+    def show(self):
+        y = "--"
+        if type(self) is BinaryTree:
+            if self.root.left_child:
+                BinaryTree.show(self.root.left_child)
+            print(str(self.root.value))
+            if self.root.right_child:
+                BinaryTree.show(self.root.right_child)
+        if type(self) is BinaryNode:
+            if self.left_child:
+                BinaryTree.show(self.left_child)
+            print(y+str(self.value))
+            if self.right_child:
+                BinaryTree.show(self.right_child)
+
+
 def visit(node: BinaryNode):
     print(node.value)
 
-def all_path(tr:BinaryNode):
+def all_path(tr:BinaryTree):
     wynik = [[]]
     x = []
     root = tr.root
     stos = deque()
-    stos.append(root, x)
+    stos.append((root, x))
     while stos:
-        x,node = stos.pop()
+        node,x = stos.pop()
+
         x.append(node)
+        if node.is_leaf():
+            wynik.append(list(x))
         if node.right_child:
             stos.append((node.right_child,list(x)))
         if node.left_child:
-            stos.append((node.left_child,list(path)))
-        if node.is_leaf():
-            wynik.append(list(path))
+            stos.append((node.left_child,list(x)))
+
     return wynik
+
+
+bn = BinaryNode(10)
+
+bn.add_left_child(9)
+bn.add_right_child(7)
+bn.left_child.add_left_child(5)
+bt = BinaryTree(bn)
+
+bt.show()
+lista = all_path(bt)
+st = ""
+for x in lista:
+    print("\n")
+    for y in x:
+        st+="["+str(y.value)+"]"
+    print(st)
+    st=""
+
+
+
+
+
 
